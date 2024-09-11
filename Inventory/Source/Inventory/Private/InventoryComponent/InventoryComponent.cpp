cd7b10584318af6474bc2aa75515da14feda6693
moad6127 <moad6127@gmail.com>
2024-09-11 16:31:36 +0900

    Feat : InventorySystem
    버그 수정
    IsRoomAvailable함수에서 IsPositionValid에 !를 추가하지 않아서 버그가 발생함 수정함
    
    +
    인벤토리에 공간이 있을경우 아이템을 저장할수 있게 만들었음

--- Inventory/Source/Inventory/Private/InventoryComponent/InventoryComponent.cpp
+++ Inventory/Source/Inventory/Private/InventoryComponent/InventoryComponent.cpp
@@ -22,13 +22,14 @@ bool UInventoryComponent::TryAddItems(UItemObject* InItem)
 		for (int j = 0; j < Columns; j++)
 		{
 			// TopLeft의 위치에 아이템이 들어올수 있는지 체크하기
-			FIntPoint Location(i, j);
+			FIntPoint Location(j,i);
 			if (IsRoomAvailable(InItem, Location))
 			{
 				//TODO : 인벤토리가 변했기 때문에 UI작업 하기
 				PlaceItem(InItem, Location);
 				InItem->SetItemLocation(Location);
 				InventoryItems.Add(InItem);
+				return true;
 			}
 		}
 	}
@@ -50,19 +51,19 @@ void UInventoryComponent::InitializeInventory()
 bool UInventoryComponent::IsRoomAvailable(UItemObject* InItem, FIntPoint InLocation)
 {
 	// BottomRight가 인벤토리 범위를 넘어가는지 확인하기
-	FIntPoint BottomRight;
-	BottomRight.X = InLocation.X + InItem->GetSizeX() - 1;
-	BottomRight.Y = InLocation.Y + InItem->GetSizeY() - 1;
-	if (IsPositionValid(BottomRight))
+	int32 BottomRightX = InLocation.X + InItem->GetSizeX();
+	int32 BottomRightY = InLocation.Y + InItem->GetSizeY();
+	FIntPoint BottomRight(BottomRightX, BottomRightY);
+	if (!IsPositionValid(BottomRight))
 	{
 		return false;
 	}
 	// TopLeft부터 아이템의 사이즈 까지 인벤토리 공간 체크
 	// EX) TopLeft가 1,1이고 아이템 Size가 3x3이면
-	// BottomRight는 1 + 3 - 1의 값을 가진다.
-	for (int i = InLocation.Y; i < InLocation.Y + InItem->GetSizeY() - 1; i++)
+	// BottomRight는 1 + 3의 값을 가진다.
+	for (int32 i = InLocation.Y; i < InLocation.Y + InItem->GetSizeY(); i++)
 	{
-		for (int j = InLocation.X; j < InLocation.X + InItem->GetSizeX() - 1; j++)
+		for (int32 j = InLocation.X; j < InLocation.X + InItem->GetSizeX(); j++)
 		{
 			//인벤토리를 순회하면서 TopLeft부터 BottomRight까지 인벤토리Grid를 확인해서
 			// Grid가 1일경우 false반환하기
@@ -78,9 +79,9 @@ bool UInventoryComponent::IsRoomAvailable(UItemObject* InItem, FIntPoint InLocat
 
 void UInventoryComponent::PlaceItem(UItemObject* InItem, FIntPoint InLocation)
 {
-	for (int i = InLocation.Y; i < InLocation.Y + InItem->GetSizeY() - 1; i++)
+	for (int32 i = InLocation.Y; i < InLocation.Y + InItem->GetSizeY(); i++)
 	{
-		for (int j = InLocation.X; j < InLocation.X + InItem->GetSizeX() - 1; j++)
+		for (int32 j = InLocation.X; j < InLocation.X + InItem->GetSizeX(); j++)
 		{
 			InventoryGrid[GetIndex(j, i)] = true;
 		}
@@ -90,8 +91,8 @@ void UInventoryComponent::PlaceItem(UItemObject* InItem, FIntPoint InLocation)
 bool UInventoryComponent::IsPositionValid(FIntPoint InLocation)
 {
 	// 아이템을 넣을때 해당 위치가 올바른지 확인
-	return InLocation.X >= 0 && InLocation.X < Columns &&
-		   InLocation.Y >= 0 && InLocation.Y < Rows;
+	return InLocation.X >= 0.f && InLocation.X < Columns &&
+		   InLocation.Y >= 0.f && InLocation.Y < Rows;
 }
 
 FIntPoint UInventoryComponent::IndexToPoint(int32 TopLeftIndex)
