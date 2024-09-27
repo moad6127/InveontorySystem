a54e584021b69af29dd88b5d38e0e0a8c89ccca0
moad6127 <moad6127@gmail.com>
2024-09-27 18:24:06 +0900

    Feat : InventorySystem
    FPSCharacter를 설정하기
    +
    FPSCharacter로 기존의 인벤토리를 열수 있도록 만들기

--- Inventory/Source/Inventory/Private/FPS/SInvenFPSCharacter.cpp
+++ Inventory/Source/Inventory/Private/FPS/SInvenFPSCharacter.cpp
@@ -2,4 +2,51 @@
 
 
 #include "FPS/SInvenFPSCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InventoryComponent/SInventoryComponent.h"
+#include "InventoryHUD.h"
+
+
+ASInvenFPSCharacter::ASInvenFPSCharacter()
+{
+	SInventoryComponent = CreateDefaultSubobject<USInventoryComponent>("SInventoryComponent");
+}
+
+void ASInvenFPSCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+	HUD = Cast<AInventoryHUD>(PlayerController->GetHUD());
+
+	if (HUD)
+	{
+		HUD->InitHUD(PlayerController, SInventoryComponent);
+	}
+}
+
+void ASInvenFPSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* PlayerEnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		PlayerEnhancedInputComponent->BindAction(InventoryAction, ETriggerEvent::Completed, this, &ASInvenFPSCharacter::ShowInventory);
+	}
+}
+
+void ASInvenFPSCharacter::ShowInventory()
+{
+	if (HUD)
+	{
+		HUD->ShowInventory();
+	}
+}
+
+
+
+bool ASInvenFPSCharacter::AddItem(UItemObject* InItem)
+{
+	return SInventoryComponent->TryAddItems(InItem);
+}
 
--- Inventory/Source/Inventory/Public/FPS/SInvenFPSCharacter.h
+++ Inventory/Source/Inventory/Public/FPS/SInvenFPSCharacter.h
@@ -4,14 +4,39 @@
 
 #include "CoreMinimal.h"
 #include "FPSCharacter.h"
+#include "Interface/InventoryPlayerInterface.h"
 #include "SInvenFPSCharacter.generated.h"
 
 /**
  * 
  */
+
+class AInventoryHUD;
+class USInventoryComponent;
+
 UCLASS()
-class INVENTORY_API ASInvenFPSCharacter : public AFPSCharacter
+class INVENTORY_API ASInvenFPSCharacter : public AFPSCharacter, public IInventoryPlayerInterface
 {
 	GENERATED_BODY()
-	
+
+	virtual bool AddItem(UItemObject* InItem) override;
+
+protected:
+
+	ASInvenFPSCharacter();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Inventory, meta = (AllowPrivateAccess = "true"))
+	USInventoryComponent* SInventoryComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* InventoryAction;
+
+	UPROPERTY()
+	AInventoryHUD* HUD;
+
+	virtual void BeginPlay() override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void ShowInventory();
 };
